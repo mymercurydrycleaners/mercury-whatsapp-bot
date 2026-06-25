a0e62ce2a7d55b7147cec90ae06827d2ea898322
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-25 14:23:09 +0530

    Update conversation.js

--- conversation.js
+++ conversation.js
@@ -23,7 +23,7 @@ const SHOP_TIMING =
   "🕒 *Shop Timing*\nMonday - Saturday: 9:00 AM - 8:00 PM\nSunday: 10:00 AM - 2:00 PM";
 
 const SHOP_LOCATION =
-  "📍 *Our Location*\nMy Mercury Dry Cleaners\n[1st floor, Infornt of shukwari bazar]\n[Near old Pvt Bus Stand, Gandhi nagar]\n[Mahoba, Uttar Pradesh - pincode- 210427]\n\nGoogle Maps: [paste your Google Maps link here]";
+  "📍 *Our Location*\nMy Mercury Dry Cleaners\n[1st floor, Infornt of shukwari bazar]\n[Near old Pvt Bus Stand, Gandhi nagar]\n[Mahoba, Uttar Pradesh - pincode- 210427]\n\nGoogle Maps: [https://maps.app.goo.gl/7tZu9Dq4LYmiZMHx6]";
 
 const SERVICES_TEXT =
   "🧺 *Our Services*\n\n• Dry Cleaning\n• Laundry\n• Steam Iron\n\nReply *4* to see our full price list.";
